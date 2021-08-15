7fcef859cc9efd5e1f8323004b1282c5d9b2f0c0
wingchiehpih <wingchiehpih@gmail.com>
2021-08-15 15:19:50 +0800

    Carousel完成

--- src/pages/discover/c-pages/recommend/index.js
+++ src/pages/discover/c-pages/recommend/index.js
@@ -1,32 +1,36 @@
-import React, { memo, useEffect } from 'react';
+import React, { memo } from 'react';
+// import { useEffect } from 'react';
 // import {connect} from 'react-redux'
-import { shallowEqual, useDispatch, useSelector } from 'react-redux';
+// import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 
-import { getTopBannerAction } from './store/actionCreators';
+// import { getTopBannerAction } from './store/actionCreators';
+import TopBanner from './c-cpns/top-banner';
+import { RecommendWrapper } from './style';
 
 // store中使用redux-immutable合并代码后，需要通过immutable中的get方式拿到recommend对象，immutable优化后代码
 // 使用hooks重构
 function Recommend() {
-  // 组件和redux关联，获取数据和进行操作
-  // 通过hooks拿到store中的数据
-  // shallowEqual将state中的数据进行浅层比较，提高性能
-  const { topBanners } = useSelector(
-    state => ({
-      // topBanners: state.get('recommend').get('topBanners'),
-      // getIn()传入一个可迭代对象，先取第一层recommend，再取第二层topBanners
-      topBanners: state.getIn(['recommend', 'topBanners']),
-    }),
-    shallowEqual
-  );
-  const dispatch = useDispatch();
-  // 括号内的dispatch：当dispatch发生改变后就重新发送网络请求
-  useEffect(() => {
-    dispatch(getTopBannerAction());
-  }, [dispatch]);
+  // 以下代码移入c - cpns中的top - banner;
+  // // 组件和redux关联，获取数据和进行操作
+  // // 通过hooks拿到store中的数据
+  // // shallowEqual将state中的数据进行浅层比较，提高性能
+  // const { topBanners } = useSelector(
+  //   state => ({
+  //     // topBanners: state.get('recommend').get('topBanners'),
+  //     // getIn()传入一个可迭代对象，先取第一层recommend，再取第二层topBanners
+  //     topBanners: state.getIn(['recommend', 'topBanners']),
+  //   }),
+  //   shallowEqual
+  // );
+  // const dispatch = useDispatch();
+  // // 括号内的dispatch：当dispatch发生改变后就重新发送网络请求
+  // useEffect(() => {
+  //   dispatch(getTopBannerAction());
+  // }, [dispatch]);
   return (
-    <div>
-      <h1>{topBanners.length}</h1>
-    </div>
+    <RecommendWrapper>
+      <TopBanner></TopBanner>
+    </RecommendWrapper>
   );
 }
 
